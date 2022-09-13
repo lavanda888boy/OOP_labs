b1e06377546289979a11f125b830f0948b4be77b
Sevastian Bajenov <sevabaj@gmail.com>
2022-09-13 18:28:56 +0300

    Add the scratch of the game's main menu.

--- Lab_1/15_game.c
+++ Lab_1/15_game.c
@@ -3,7 +3,25 @@
 
 int main(void){
 
+  // initialise the main menu of the game
+  while(1){
+    printf("Welcome to the main menu of the Game of 15!\n");
+    printf("In order to start a new game press 's';\n");
+    printf("If you wish to exit the game press 'e';\n");
 
+    char c;
+    scanf("%c", &c);
+
+    if(c == 's'){
+
+    } else if(c == 'e'){
+      printf("Exiting the game...\n");
+      break;
+    } else{
+
+    }
+
+  }
 
   return 0;
 }
