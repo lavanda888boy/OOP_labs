2c769138d52c82f3e8324e69f0e39ab20861d796
Sevastian Bajenov <sevabaj@gmail.com>
2022-09-15 16:23:09 +0300

    Add the processMatrix() function which represents the main step of the game.

--- Lab_1/15_game.c
+++ Lab_1/15_game.c
@@ -12,8 +12,8 @@ void swap(int *a, int *b){
 
 // main function representing the gaming process itself
 void startTheGame(){
-  printf("The game starts!\nTo return to the main menu press '0 0'.\n");
-  printf("Type the number you wish to move and 4, 6, 8, 2 with a single space.\n");
+  printf("The game starts!\nTo return to the main menu press 'm'.\n");
+  printf("Type the direction you wish to move the space and 4, 6, 8, 2.\n");
   printf("The second number will represent the direction of movement.\n");
   printf("Left, right, up, or down respectively.\n\n");
 
@@ -50,16 +50,60 @@ void startTheGame(){
       break;
     }
 
-    int number;
-    char direction;
-    scanf("%d %c", &number, &direction);
+    int direction;
+    scanf("%d", &direction);
 
+    int coord[2];
+
+    for(int i = 0; i < 4; i++){
+      for(int j = 0; j < 4; j++){
+        if(gameBoard[i][j] == 0){
+          coord[0] = i;
+          coord[1] = j;
+        }
+      }
+    }
+
+    processMatrix(gameBoard, direction, coord[0], coord[1]);
 
     printTheCurrentStateOfTheGame(gameBoard);
   }
 
   return;
 }
+
+
+// function which moves the white space in a certain direction
+// genrally the main action in the game
+void processMatrix(int matrix[][4], int dir, int nullRow, int nullColumn){
+  int temp;
+
+  switch(dir){
+    case 4:
+      matrix[nullRow][nullColumn] = matrix[nullRow][nullColumn - 1];
+      matrix[nullRow][nullColumn - 1] = 0;
+      break;
+
+    case 8:
+      matrix[nullRow][nullColumn] = matrix[nullRow - 1][nullColumn];
+      matrix[nullRow - 1][nullColumn] = 0;
+      break;
+
+    case 6:
+      matrix[nullRow][nullColumn] = matrix[nullRow][nullColumn + 1];
+      matrix[nullRow][nullColumn + 1] = 0;
+      break;
+
+    case 2:
+      matrix[nullRow][nullColumn] = matrix[nullRow + 1][nullColumn];
+      matrix[nullRow + 1][nullColumn] = 0;
+      break;
+
+    default:
+      printf("Wrong direction!\n");
+  }
+}
+
 
 // function for printing the matrix of numbers
 // representing the current situation in the game
