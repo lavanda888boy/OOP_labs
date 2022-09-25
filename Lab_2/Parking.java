8afc7d28b0f73dd71cc9568c0a52eef244e9b8f5
Sevastian Bajenov <sevabaj@gmail.com>
2022-09-25 13:23:27 +0300

    Add the fields and corresponding getters() to the Parking class.

---
+++ Lab_2/Parking.java
@@ -0,0 +1,45 @@
+import java.util.List;
+import java.util.LinkedList;
+
+public class Parking{
+
+  private List<Level> levels;
+  private Gate gate;
+  private Elevator elevator;
+  private PaymentTerminal paymentTerminal;
+  private ServiceManager serviceManager;
+  private CarQueue carQueue;
+
+  public Parking(Gate g, Elevator el, PaymentTerminal pt, ServiceManager sm, CarQueue cq){
+    levels = new LinkedList<>();
+    gate = g;
+    elevator = el;
+    paymentTerminal = pt;
+    serviceManager = sm;
+    carQueue = cq;
+  }
+
+  public List<Level> getLevels(){
+    return this.levels;
+  }
+
+  public Gate getGate(){
+    return this.gate;
+  }
+
+  public Elevator getElevator(){
+    return this.elevator;
+  }
+
+  public PaymentTerminal getPaymentTerminal(){
+    return this.paymentTerminal;
+  }
+
+  public ServiceManager getServiceManager(){
+    return this.serviceManager;
+  }
+
+  public CarQueue getCarQueue(){
+    return this.carQueue;
+  }
+}
